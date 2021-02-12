32bec7253224e794549f86f1c00dfa6e4b1bd165
Shane Harris <shane@sidequestvr.com>
2021-02-12 22:03:41 +0000

    first basic implementation of app lab listings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
+export interface App{
+  name: string;
+  apps_id: number;
+  users_id: number;
+  summary: string;
+  description: string;
+  app_banner: string;
+  image_url: string;
+  app_categories_id: string;
+  website: string;
+  supports_go: boolean;
+  supports_quest: boolean;
+  supports_link: boolean;
+  supports_other: boolean;
+  is_webxr: boolean;
+  early_access: boolean;
+  created: string;
+  updated: string;
+  license: string;
+  downloads: string;
+  sort_rating: string;
+  number_of_rating: string;
+  num_reviews: number|null;
+  is_app_lab: string;
+  oculus_url: string;
+  external_listing_url: string;
+  hot_sort_rating: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AppLabGames';
+
+  title = 'AppLab Games';
+  apps: App[] = [];
+
+  constructor() {
+    this.loadApps();
+  }
+
+  async loadApps(): Promise<void> {
+    try {
+      const resp = await fetch('https://api.sidequestvr.com/v2/apps?limit=200&is_app_lab=true&has_oculus_url=true&sortOn=hot_sort_rating');
+      this.apps = await resp.json();
+    } catch (e) {
+      alert(e);
+    }
+  }
 }
